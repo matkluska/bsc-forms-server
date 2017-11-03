017d3b5af75882490f897509aae98154b4213d0e
mkluska <mkluska@sointeractive.pl>
2017-11-03 15:16:09 +0100

    Add long stats query.

--- reply-stats-service/src/main/java/io/kluska/bsc/forms/reply/stats/service/domain/model/stats/LongTextStats.java
+++ reply-stats-service/src/main/java/io/kluska/bsc/forms/reply/stats/service/domain/model/stats/LongTextStats.java
@@ -1,7 +1,6 @@
 package io.kluska.bsc.forms.reply.stats.service.domain.model.stats;
 
 import io.kluska.bsc.forms.reply.stats.service.domain.model.QuestionStats;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -11,8 +10,7 @@ import java.util.List;
  * @author Mateusz Kluska
  */
 @Data
-@Builder
 @EqualsAndHashCode(callSuper = false)
 public class LongTextStats extends QuestionStats {
-    private List<String> replies;
+	private List<String> replies;
 }
--- reply-stats-service/src/main/java/io/kluska/bsc/forms/reply/stats/service/domain/repository/StatsRepositoryMongoImpl.java
+++ reply-stats-service/src/main/java/io/kluska/bsc/forms/reply/stats/service/domain/repository/StatsRepositoryMongoImpl.java
@@ -2,13 +2,16 @@ package io.kluska.bsc.forms.reply.stats.service.domain.repository;
 
 import io.kluska.bsc.forms.form.api.dto.FormDTO;
 import io.kluska.bsc.forms.form.api.dto.QuestionDTO;
+import io.kluska.bsc.forms.form.api.dto.question.LongTextDTO;
 import io.kluska.bsc.forms.form.api.dto.question.ShortTextDTO;
 import io.kluska.bsc.forms.form.api.dto.question.SingleChoiceDTO;
 import io.kluska.bsc.forms.reply.stats.service.domain.model.FormStats;
 import io.kluska.bsc.forms.reply.stats.service.domain.model.QuestionStats;
 import io.kluska.bsc.forms.reply.stats.service.domain.model.Reply;
+import io.kluska.bsc.forms.reply.stats.service.domain.model.replies.LongTextReply;
 import io.kluska.bsc.forms.reply.stats.service.domain.model.replies.ShortTextReply;
 import io.kluska.bsc.forms.reply.stats.service.domain.model.replies.SingleChoiceReply;
+import io.kluska.bsc.forms.reply.stats.service.domain.model.stats.LongTextStats;
 import io.kluska.bsc.forms.reply.stats.service.domain.model.stats.ShortTextStats;
 import io.kluska.bsc.forms.reply.stats.service.domain.model.stats.SingleChoiceStats;
 import io.kluska.bsc.forms.reply.stats.service.domain.repository.results.OptionIdCount;
@@ -20,6 +23,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -42,64 +46,93 @@ import static org.springframework.data.mongodb.core.aggregation.Aggregation.proj
 @Repository
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class StatsRepositoryMongoImpl implements StatsRepository {
-    @NonNull
-    private final MongoTemplate mongoTemplate;
+	@NonNull
+	private final MongoTemplate mongoTemplate;
 
-    @Override
-    public FormStats getFormStats(@NonNull final String formId, @NonNull final FormDTO formDTO) {
-        List<QuestionStats> questionStats = formDTO.getQuestions().stream()
-                .filter(questionDTO -> questionDTO instanceof ShortTextDTO || questionDTO instanceof SingleChoiceDTO)
-                .map(q -> getQuestionStats(q, formId))
-                .collect(Collectors.toList());
-        return FormStats.builder()
-                .questionStats(questionStats)
-                .build();
-    }
+	@Override
+	public FormStats getFormStats(@NonNull final String formId, @NonNull final FormDTO formDTO) {
+		List<QuestionStats> questionStats = formDTO.getQuestions().stream()
+				.filter(questionDTO -> questionDTO instanceof ShortTextDTO || questionDTO instanceof SingleChoiceDTO
+						|| questionDTO instanceof LongTextDTO)
+				.map(q -> getQuestionStats(q, formId))
+				.collect(Collectors.toList());
+		return FormStats.builder()
+				.questionStats(questionStats)
+				.build();
+	}
 
-    private QuestionStats getQuestionStats(QuestionDTO questionDTO, String formId) {
-        return Match(questionDTO).of(
-                Case($(instanceOf(ShortTextDTO.class)), () -> getShortTextStats((ShortTextDTO) questionDTO, formId)),
-//                Case($(instanceOf(LongTextDTO.class)), () -> modelMapper.map(questionDTO, LongText.class)),
-                Case($(instanceOf(SingleChoiceDTO.class)), () -> getSingleChoiceStats((SingleChoiceDTO) questionDTO, formId)),
-//                Case($(instanceOf(MultipleChoiceDTO.class)), () -> modelMapper.map(questionDTO, MultipleChoice.class)),
-//                Case($(instanceOf(LinearScaleDTO.class)), () -> modelMapper.map(questionDTO, LinearScale.class)),
-                Case($(), () -> {
-                    throw new RuntimeException("Unsupported question type");
-                }));
-    }
+	private QuestionStats getQuestionStats(QuestionDTO questionDTO, String formId) {
+		return Match(questionDTO).of(
+				Case($(instanceOf(ShortTextDTO.class)), () -> getShortTextStats((ShortTextDTO) questionDTO, formId)),
+				Case($(instanceOf(LongTextDTO.class)), () -> getLongTextStats((LongTextDTO) questionDTO, formId)),
+				Case($(instanceOf(SingleChoiceDTO.class)), () -> getSingleChoiceStats((SingleChoiceDTO) questionDTO, formId)),
+				//                Case($(instanceOf(MultipleChoiceDTO.class)), () -> modelMapper.map(questionDTO, MultipleChoice.class)),
+				//                Case($(instanceOf(LinearScaleDTO.class)), () -> modelMapper.map(questionDTO, LinearScale.class)),
+				Case($(), () -> {
+					throw new RuntimeException("Unsupported question type");
+				}));
+	}
 
-    private ShortTextStats getShortTextStats(@NonNull final ShortTextDTO shortTextDTO, @NonNull final String formId) {
-        Aggregation agg = newAggregation(match(new Criteria("formId").is(formId)),
-                match(new Criteria("questionId").is(shortTextDTO.getId())),
-                count().as("value"));
-        AggregationResults<RepliesCount> aggResult = mongoTemplate.aggregate(agg, ShortTextReply.class, RepliesCount.class);
-        RepliesCount repliesCount = aggResult.getUniqueMappedResult();
+	private ShortTextStats getShortTextStats(@NonNull final ShortTextDTO shortTextDTO, @NonNull final String formId) {
+		Aggregation agg = newAggregation(match(new Criteria("formId").is(formId)),
+				match(new Criteria("questionId").is(shortTextDTO.getId())),
+				count().as("value"));
+		AggregationResults<RepliesCount> aggResult = mongoTemplate.aggregate(agg, ShortTextReply.class, RepliesCount.class);
+		RepliesCount repliesCount = aggResult.getUniqueMappedResult();
 
-        ShortTextStats shortTextStats = new ShortTextStats();
-        shortTextStats.setRepliesCount(repliesCount.getValue());
-        return shortTextStats;
-    }
+		List<String> distinctReplies = getDistinctReplies(shortTextDTO.getId(), formId);
 
-    private SingleChoiceStats getSingleChoiceStats(@NonNull final SingleChoiceDTO singleChoiceDTO, @NonNull final String formId) {
-        Aggregation agg = newAggregation(match(new Criteria("formId").is(formId)),
-                match(new Criteria("questionId").is(singleChoiceDTO.getId())),
-                group("optionId").count().as("count"),
-                project("count").and("optionId").previousOperation());
-        AggregationResults<OptionIdCount> aggResult = mongoTemplate.aggregate(agg, SingleChoiceReply.class, OptionIdCount.class);
-        Map<String, Long> optionIdToCount = aggResult.getMappedResults().stream()
-                .collect(Collectors.toMap(OptionIdCount::getOptionId, OptionIdCount::getCount));
+		ShortTextStats shortTextStats = new ShortTextStats();
+		shortTextStats.setRepliesCount(repliesCount.getValue());
+		shortTextStats.setReplies(distinctReplies);
+		return shortTextStats;
+	}
 
-        SingleChoiceStats singleChoiceStats = new SingleChoiceStats();
-        singleChoiceStats.setRepliesCount(getRepliesCount(formId, singleChoiceDTO.getId()));
-        singleChoiceStats.setOptionIdToRepliesCounts(optionIdToCount);
-        return singleChoiceStats;
-    }
+	private LongTextStats getLongTextStats(@NonNull final LongTextDTO longTextDTO, @NonNull final String formId) {
+		Aggregation agg = newAggregation(match(new Criteria("formId").is(formId)),
+				match(new Criteria("questionId").is(longTextDTO.getId())),
+				count().as("value"));
+		AggregationResults<RepliesCount> aggResult = mongoTemplate.aggregate(agg, LongTextReply.class, RepliesCount.class);
+		RepliesCount repliesCount = aggResult.getUniqueMappedResult();
 
-    private long getRepliesCount(@NonNull final String formId, @NonNull final String questionId) {
-        Aggregation agg = newAggregation(match(new Criteria("formId").is(formId)),
-                match(new Criteria("questionId").is(questionId)),
-                count().as("value"));
-        AggregationResults<RepliesCount> aggResult = mongoTemplate.aggregate(agg, Reply.class, RepliesCount.class);
-        return aggResult.getUniqueMappedResult().getValue();
-    }
+		List<String> distinctReplies = getDistinctReplies(longTextDTO.getId(), formId);
+
+		ShortTextStats shortTextStats = new ShortTextStats();
+		shortTextStats.setRepliesCount(repliesCount.getValue());
+		LongTextStats longTextStats = new LongTextStats();
+		longTextStats.setRepliesCount(repliesCount.getValue());
+		longTextStats.setReplies(distinctReplies);
+		return longTextStats;
+	}
+
+	private SingleChoiceStats getSingleChoiceStats(@NonNull final SingleChoiceDTO singleChoiceDTO, @NonNull final String formId) {
+		Aggregation agg = newAggregation(match(new Criteria("formId").is(formId)),
+				match(new Criteria("questionId").is(singleChoiceDTO.getId())),
+				group("optionId").count().as("count"),
+				project("count").and("optionId").previousOperation());
+		AggregationResults<OptionIdCount> aggResult = mongoTemplate.aggregate(agg, SingleChoiceReply.class, OptionIdCount.class);
+		Map<String, Long> optionIdToCount = aggResult.getMappedResults().stream()
+				.collect(Collectors.toMap(OptionIdCount::getOptionId, OptionIdCount::getCount));
+
+		SingleChoiceStats singleChoiceStats = new SingleChoiceStats();
+		singleChoiceStats.setRepliesCount(getRepliesCount(formId, singleChoiceDTO.getId()));
+		singleChoiceStats.setOptionIdToRepliesCounts(optionIdToCount);
+		return singleChoiceStats;
+	}
+
+	private long getRepliesCount(@NonNull final String formId, @NonNull final String questionId) {
+		Aggregation agg = newAggregation(match(new Criteria("formId").is(formId)),
+				match(new Criteria("questionId").is(questionId)),
+				count().as("value"));
+		AggregationResults<RepliesCount> aggResult = mongoTemplate.aggregate(agg, Reply.class, RepliesCount.class);
+		return aggResult.getUniqueMappedResult().getValue();
+	}
+
+	private List<String> getDistinctReplies(@NonNull final String questionId, @NonNull final String formId) {
+		Query query = new Query();
+		query.addCriteria(new Criteria("formId").is(formId));
+		query.addCriteria(new Criteria("questionId").is(questionId));
+
+		return mongoTemplate.getCollection(Reply.REPLIES_COLLECTION).distinct("option", query.getQueryObject());
+	}
 }
